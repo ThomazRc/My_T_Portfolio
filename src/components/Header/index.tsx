9dd3135c3b18bb87f28af024449b8c04f0948d05
Thomaz Rosseti <tocorosseti@gmail.com>
2024-06-04 13:07:52 -0300

    feat: initial development of header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,17 @@
-import { StyledHeader } from "./style"
+import { Link } from "react-router-dom";
+import { StyledHeader } from "./style";
 
 export const Header = () => {
-    return (
-        <StyledHeader>
-            <h1>Header</h1>
-        </StyledHeader>
-    )
-}
+  return (
+    <StyledHeader>
+      <nav>
+        <img src="/src/assets/logoT.png" alt="Logo-Assinatura" />
+        <ul>
+          <li>
+            <Link to="/">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </StyledHeader>
+  );
+};
