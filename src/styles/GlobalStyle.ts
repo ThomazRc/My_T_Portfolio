7ec90ae292eeafb15c8acde50e15c316cc687317
Thomaz Rosseti <tocorosseti@gmail.com>
2024-06-06 12:50:23 -0300

    style: initial configuration of GlobalStyle

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -3,6 +3,19 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 
 :root {
+   --primary-color: #EAEAEA
+   --secondary-color: #080808
+}
 
+body {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    border: none;
+    list-style: none;
+    text-decoration: none;
+    width: 100vw;
+    height: 100vh;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
 }
 `
