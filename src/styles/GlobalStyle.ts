d82273e2c7aadf922bb33b6f97f7f906a57cbf6e
Thomaz Rosseti <tocorosseti@gmail.com>
2024-06-07 19:19:59 -0300

    style: main, header, footer and button global style update

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -3,8 +3,8 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 
 :root {
-   --primary-color: #EAEAEA
-   --secondary-color: #080808
+   --primary-color: #EAEAEA;
+   --secondary-color: #080808;
 }
 
 body {
@@ -17,5 +17,31 @@ body {
     width: 100vw;
     height: 100vh;
     font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    background-color: var(--primary-color)
 }
+
+main {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+
+header {
+    height: 12.5rem;
+
+}
+
+footer {
+    height: 12.5rem;
+
+}
+
+button {
+    cursor: pointer;
+
+}
+
 `
