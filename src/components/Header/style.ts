ccdd62653ddf26dd56117b7a29ba05c3d6787b30
Thomaz Rosseti <tocorosseti@gmail.com>
2024-06-11 11:17:38 -0300

    style: header style update

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,10 +1,43 @@
 import styled from "styled-components";
 
 export const StyledHeader = styled.header`
-border-top: 1px solid var(--primary);
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 1px solid var(--secondary-color);
+
+  nav {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    width: 100%;
+
+  }
+
+  img {
+    width: 110px;
+    height: 110px;
+  }
+
+  ul {
+    list-style: none;
+    font-weight: bold;
+    padding: 0;
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--secondary-color);
+  }
+
+  a:hover {
+    color: var(--secondary-color);
+    transition: 0.4s;
+  }
+
+
+
+
 
 `
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,6 +5,9 @@ export const HomePage = () => {
     return (
         <>
         <Header />
+        <main>
+            <h1>main</h1>
+        </main>
         <Footer />
         </>
     )
