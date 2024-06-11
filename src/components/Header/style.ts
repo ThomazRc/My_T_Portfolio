6363f3a404cec6a4f224f9e4ab4d9508f0dcac4f
Thomaz Rosseti <tocorosseti@gmail.com>
2024-06-10 23:21:55 -0300

    style: initial stilization of header component

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,4 +1,10 @@
 import styled from "styled-components";
 
 export const StyledHeader = styled.header`
+border-top: 1px solid var(--primary);
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+
 `
