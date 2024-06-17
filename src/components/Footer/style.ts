0a9bd2dc9cd58b3f9f45d409904d16385ae67cae
Thomaz Rosseti <tocorosseti@gmail.com>
2024-06-17 12:10:36 -0300

    style: footer component initial style

--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -1,4 +1,18 @@
 import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
-`
+  border-top: 1px solid var(--secondary-color);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  h3 {
+    margin-bottom: 1.2em;
+  }
+
+  span {
+    font-weight: bold;
+    color: var(--secondary-color);
+  }
+`;
